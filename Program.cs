2a6f17f8fdb6228b174e2ea54b06682d42ddff91
Nathan George <kosmosisdire@gmail.com>
2023-04-05 21:48:17 -0400

    fix url

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 var listener = new BackendListener(45223, 3, 5840, "/home/cse-user/backend/data/");
 listener.Start();
 
-WebSocketServer.RunServer("192.168.1.2:45224/");
+WebSocketServer.RunServer("https://nathang.dev:45224/");
 
 Console.WriteLine("Press any key to stop the server...");
 Console.ReadKey();
