32766a2349849a13ce61ace67c033cdb505994cc
Nathan George <kosmosisdire@gmail.com>
2023-04-05 21:23:19 -0400

    start impl. of websocket server and client

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 var listener = new BackendListener(45223, 3, 5840, "/home/cse-user/backend/data/");
 listener.Start();
 
+WebSocketServer.RunServer("http://nathang.dev:45224/");
+
 Console.WriteLine("Press any key to stop the server...");
 Console.ReadKey();
 
---
+++ WebSocketServer.cs
@@ -0,0 +1,63 @@
+
+
+using System;
+using System.Net;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+public class WebSocketServer
+{
+    public static async void RunServer(string url)
+    {
+        HttpListener listener = new HttpListener();
+        listener.Prefixes.Add(url);
+        listener.Start();
+
+        while (true)
+        {
+            HttpListenerContext context = await listener.GetContextAsync();
+            if (context.Request.IsWebSocketRequest)
+            {
+                HttpListenerWebSocketContext webSocketContext = await context.AcceptWebSocketAsync(subProtocol: null);
+
+                // Handle the WebSocket connection
+                await HandleWebSocket(webSocketContext.WebSocket);
+            }
+            else
+            {
+                context.Response.StatusCode = 400;
+                context.Response.Close();
+            }
+        }
+    }
+
+    private static async Task HandleWebSocket(WebSocket webSocket)
+    {
+        byte[] buffer = new byte[1024];
+
+        while (webSocket.State == WebSocketState.Open)
+        {
+            WebSocketReceiveResult result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            if (result.MessageType == WebSocketMessageType.Text)
+            {
+                string request = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+                // Process the request and send a response
+                string response = ProcessRequest(request);
+                byte[] responseBuffer = System.Text.Encoding.UTF8.GetBytes(response);
+                await webSocket.SendAsync(new ArraySegment<byte>(responseBuffer), WebSocketMessageType.Text, true, CancellationToken.None);
+            }
+            else if (result.MessageType == WebSocketMessageType.Close)
+            {
+                await webSocket.CloseAsync(result.CloseStatus.Value, result.CloseStatusDescription, CancellationToken.None);
+            }
+        }
+    }
+
+    private static string ProcessRequest(string request)
+    {
+        // Process the request and return a response
+        return "Response to " + request;
+    }
+}
