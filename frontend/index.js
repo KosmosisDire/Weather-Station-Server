53d0b4071f7f218367c7c01553ffa1ef03009875
cse-user <kosmosisDire@gmail.com>
2023-04-13 02:02:08 +0000

    Update styling, auto update

--- frontend/index.js
+++ frontend/index.js
@@ -1,103 +1,174 @@
-// import Chart from 'chart.js'
-
 const initSqlJs = window.initSqlJs;
-const SQL = await initSqlJs({
-    // Required to load the wasm binary asynchronously. Of course, you can host it wherever you want
-    // You can omit locateFile completely when running in node
-    locateFile: file => `https://nathang.dev/cse590/frontend/${file}`
-});
+const SQL = await initSqlJs({locateFile: file => `https://nathang.dev/cse590/frontend/${file}`});
 
-const databaseBuffer = await fetch('https://nathang.dev/cse590/data/database.db').then(response => response.arrayBuffer());
-const database = new SQL.Database(new Uint8Array(databaseBuffer));
+let tempChart = null;
+let pressureChart = null;
 
-const query = 'SELECT * FROM sensor WHERE clientName = "Nathan"';
-let results = database.exec(query);
-let rows = results[0].values;
-
-let timestamps = rows.map(row => new Date(row[0] * 1000).toTimeString());
-let temperatures = rows.map(row => row[2]);
-let pressures = rows.map(row => row[3]);
-
-for (let i = 1; i < rows.length; i++) 
+async function loadData()
 {
-    if (temperatures[i] == 12345678)
+    const databaseBuffer = await fetch('https://nathang.dev/cse590/data/database.db').then(response => response.arrayBuffer());
+    const database = new SQL.Database(new Uint8Array(databaseBuffer));
+
+    // get unique client names and timestamps
+    let query = 'SELECT timestamp, clientName FROM sensor LIMIT 8640';
+    let results = database.exec(query);
+    let rows = results[0].values;
+    let uniqueClientNames = [...new Set(rows.map(row => row[1]))];
+    let timestamps = rows.map(row => new Date(row[0] * 1000).toTimeString().split(' ')[0].split(':').slice(0, 2).join(':'));
+
+
+
+    // select data for each client
+    let pressureData = [];
+    let temperatureData = [];
+
+    for (let i = 0; i < uniqueClientNames.length; i++)
     {
-        temperatures[i] = temperatures[i - 1];
+        query = `SELECT * FROM sensor WHERE clientName = '${uniqueClientNames[i]}'`;
+        results = database.exec(query);
+        rows = results[0].values;
+
+        
+        let temperatures = rows.map(row => row[2]);
+        let pressures = rows.map(row => row[3]/100);
+
+        for (let i = 1; i < rows.length; i++) 
+        {
+            if (temperatures[i] == -1234567)
+            {
+                temperatures[i] = temperatures[i - 1];
+            }
+
+            if (pressures[i] == -1234567/100)
+            {
+                pressures[i] = pressures[i - 1];
+            }
+        }
+
+        pressureData.push({
+            label: uniqueClientNames[i],
+            data: pressures
+        });
+
+        temperatureData.push({
+            label: uniqueClientNames[i],
+            data: temperatures
+        });
     }
 
-    if (pressures[i] == 12345678)
+
+    function hexToRgb(hex) 
     {
-        pressures[i] = pressures[i - 1];
+        hex = hex.toLowerCase().trim();
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+
+        if (!result) {
+            console.error("Invalid hex color code: " + hex);
+            return null;
+        }
+
+        return "rgb(" + parseInt(result[1], 16) + "," + parseInt(result[2], 16) + "," + parseInt(result[3], 16) + ")";
+    }
+
+
+    function getCssVariableValue(variableName) 
+    {
+        return getComputedStyle(document.body).getPropertyValue(variableName).trim();
+    }
+
+    console.log(Chart.defaults);
+    Chart.defaults.interaction.mode = 'nearest';
+    Chart.defaults.interaction.axis = 'x';
+    Chart.defaults.interaction.intersect = false;
+    Chart.defaults.datasets.line.pointRadius = 0;
+    Chart.defaults.maintainAspectRatio = false;
+    Chart.defaults.color = hexToRgb(getCssVariableValue('--text-color'));
+    Chart.defaults.scale.grid.color = hexToRgb(getCssVariableValue('--text-color-muted'));
+
+    if (tempChart == null)
+    {
+        tempChart = new Chart(
+            document.getElementById('temperature-chart'),
+            {
+                type: 'line',
+                data: {
+                    labels: timestamps,
+                    datasets: temperatureData
+                },
+                options: {
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Temperature over 24 hours'
+                        }
+                    },
+                    scales: {
+                        y: {
+                            ticks: {
+                                // Include a dollar sign in the ticks
+                                callback: function(value, index, ticks) {
+                                    return value + '°C';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        );
+
+        console.log(tempChart);
+    }
+    else
+    {
+        tempChart.data.datasets = temperatureData;
+        tempChart.data.labels = timestamps;
+        tempChart.options.animation = false;
+        tempChart.update();
+    }
+
+    if (pressureChart == null)
+    {
+        pressureChart = new Chart(
+            document.getElementById('pressure-chart'),
+            {
+                type: 'line',
+                data: {
+                    labels: timestamps,
+                    datasets: pressureData
+                },
+                options: {
+                    plugins: {
+                        title: {
+                            display: true,
+                            text: 'Pressure over 24 hours'
+                        }
+                    },
+                    scales: {
+                        y: {
+                            ticks: {
+                                // Include a dollar sign in the ticks
+                                callback: function(value, index, ticks) {
+                                    return value + ' hPa';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        );
+    }
+    else
+    {
+        pressureChart.data.datasets = pressureData;
+        pressureChart.data.labels = timestamps;
+        pressureChart.options.animation = false;
+        pressureChart.update();
     }
 }
 
-new Chart(
-    document.getElementById('temperature-chart'),
-    {
-        type: 'bar',
-        data: {
-            labels: timestamps,
-            datasets: [
-                {
-                    label: 'Pressure over time',
-                    data: pressures,
-                },
-                {
-                    label: 'Temperature over time',
-                    data: temperatures,
-                }
-            ]
-        }
-    }
-);
+await loadData();
 
-
-
-// async function waitUntil(conditionFunction, interval = 200, debugFunction = undefined) 
-// {
-//     const poll = resolve => 
-//     {
-//         if(debugFunction) debugFunction();
-//         if(conditionFunction()) resolve();
-//         else setTimeout(_ => poll(resolve), interval);
-//     }
-
-//     return new Promise(poll);
-// }
-
-
-// // Open a WebSocket connection
-// var ws = new WebSocket('wss://nathang.dev:45224');
-
-// ws.onerror = function(event) 
-// {
-//     console.error('WebSocket error:', event);
-// };
-
-// console.log('Connecting to server...');
-
-// // Wait until the WebSocket is open
-// await waitUntil(() => ws.readyState === ws.OPEN, 200, () => console.log("ws State is: " + ws.readyState));
-
-// console.log('Connected to server!');
-
-// // Send a message containing the request for the object array
-// ws.send('get-object-array');
-
-// console.log('Sent request for object array!');
-
-// // Listen for incoming messages from the server
-// ws.onmessage = function(event) 
-// {
-//     // Parse the incoming message as JSON
-//     var message = JSON.parse(event.data);
-
-//     console.log('Received message from server!');
-
-//     // Check if the message is a response to the object array request
-//     if (message.type === 'object-array') {
-//         // Do something with the object array
-//         var objectArray = message.data;
-//         console.log(objectArray);
-//     }
-// };
+setInterval(async () => 
+{
+    await loadData();
+}, 10000);
